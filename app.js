16c0bdb3c2033103b0625103da443e44f69d13c9
Glenn Tippett <glenn.tippett@gmail.com>
2021-12-23 17:38:35 +1100

    refactor

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const crypto = require("crypto");
 const prompt = require("prompt-sync")();
 
 function md5Hash(amount) {
-  console.log(amount);
   console.log("> Computing MD5 hash...");
   console.log(crypto.createHash("md5").update(amount).digest("hex"));
 }
